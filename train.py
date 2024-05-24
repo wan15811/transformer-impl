b5abd715e18afc033f82d5d0b20ef3328176dca2
Wan <58760263+wan15811@users.noreply.github.com>
2024-05-24 21:11:38 +0700

    Update train.py

--- train.py
+++ train.py
@@ -146,10 +146,9 @@ def get_ds(config):
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
 
-    # Keep 40% for training, 10% for validation
     total_size = int(len(ds_raw))
-    train_ds_size = int(0.1 * len(ds_raw))
-    val_ds_size = int(0.02 * len(ds_raw))
+    train_ds_size = int(0.04 * len(ds_raw))
+    val_ds_size = int(0.01 * len(ds_raw))
     remaining_size = total_size - train_ds_size - val_ds_size
     train_ds_raw, val_ds_raw, _ = random_split(ds_raw, [train_ds_size, val_ds_size, remaining_size])
     
