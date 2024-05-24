947cd179eac71c014bef40e707ce609529de9279
Wan <58760263+wan15811@users.noreply.github.com>
2024-05-24 20:35:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ def get_config():
         "batch_size": 8,
         "num_epochs": 20,
         "lr": 10**-4,
-        "seq_len": 350,
+        "seq_len": 500,
         "d_model": 512,
         "datasource": 'mt_eng_vietnamese',
         "lang_src": "en",
