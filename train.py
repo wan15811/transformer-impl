df9dc913d999c6ed85d9ccbbfcf71ac938824e76
Wan <58760263+wan15811@users.noreply.github.com>
2024-05-24 20:53:26 +0700

    Update train.py

--- train.py
+++ train.py
@@ -146,10 +146,13 @@ def get_ds(config):
     tokenizer_src = get_or_build_tokenizer(config, ds_raw, config['lang_src'])
     tokenizer_tgt = get_or_build_tokenizer(config, ds_raw, config['lang_tgt'])
 
-    # Keep 90% for training, 10% for validation
-    train_ds_size = int(0.9 * len(ds_raw))
-    val_ds_size = len(ds_raw) - train_ds_size
+    # Keep 40% for training, 10% for validation
+    train_ds_size = int(0.4 * len(ds_raw))
+    val_ds_size = int(0.1 * len(ds_raw))
     train_ds_raw, val_ds_raw = random_split(ds_raw, [train_ds_size, val_ds_size])
+    
+    print(f"Training set size: {len(train_ds_raw)}")
+    print(f"Validation set size: {len(val_ds_raw)}")    
 
     train_ds = BilingualDataset(train_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
     val_ds = BilingualDataset(val_ds_raw, tokenizer_src, tokenizer_tgt, config['lang_src'], config['lang_tgt'], config['seq_len'])
